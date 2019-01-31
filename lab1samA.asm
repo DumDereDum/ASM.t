c32d759298afac6fc2c514d168724178adeda42d
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-01-31 21:22:00 +0300

    Update lab1samA.asm

--- lab1samA.asm
+++ lab1samA.asm
@@ -33,7 +33,7 @@ begin:
 	mov dx, offset filename+2
 	int 21h
 	jc error1
-a:	
+	
 	mov handle, ax
 	mov bx, ax
 	mov ah, 3Fh
@@ -42,26 +42,21 @@ a:
 	int 21h
 	jc error2
 	
-	mov ah, 3Eh
-	mov bx, offset handle
-	int 21h
-	
+a:	
 	mov si, offset buffer
-	mov dx, cx
+	mov di, cx
 	
 cycle:
 	lodsb
 	call change_and_print
 	loop cycle
-
+	
 	jmp press_button2
 
 continue:
 	
 	cmp di, 1999
 	jna off
-	cmp ax, 1C0Dh
-	jne continue
 	mov ax, 4201h
 	mov bx, handle
 	mov cx, 0
@@ -70,16 +65,11 @@ continue:
 
 	jmp a
 	
-	
-	
 	jmp off
 	
 press_button1:
 
-	mov dx, offset msg_press_ESC
-	call print 
-	mov dx, offset msg_press_Space
-	call print 
+	call print_ECS_Space
 	
 	call press_button
 	
@@ -96,10 +86,7 @@ press_button2:
 	push ax
 	push dx
 	
-	mov dx, offset msg_press_ESC
-	call print 
-	mov dx, offset msg_press_Space
-	call print 
+	call print_ECS_Space
 	
 	call press_button
 	
@@ -141,6 +128,16 @@ off:
 	int 20h
 
 ;----------Пoдпрограммы----------
+
+print_ECS_Space proc
+	
+	mov dx, offset msg_press_ESC
+	call print 
+	mov dx, offset msg_press_Space
+	call print 
+	
+	ret
+print_ECS_Space endp
 
 print proc
 	mov ah,09h
@@ -200,7 +197,6 @@ change_and_print endp
 	filename db 40 dup (' ')
 	buffer dw 2000 dup(' ')
 	handle dw 0
-	length_block dw 0
 ;----------Сообщения---------	
 	msg_error_file db ' Error: file is not found', 0Ah, 0Dh, '$'
 	msg_error_buffer db ' Error: can not read file', 0Ah, 0Dh, '$'
